12561b79dacc4c3c74b273dccfa34f247ca2340f
safaet <safaetjaman@gmail.com>
2025-03-02 14:17:43 +0600

    add new post

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import post_list  # Import our view function
+from .views import post_list, create_post  # Import our view function
 
 urlpatterns = [
     path('', post_list, name='post_list'),  # Map the root URL to the post_list view
+    path('new/', create_post, name='create_post'),  # Page to create new posts
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # View function to display all blog posts
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+        
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/create_post.html', {'form': form})
